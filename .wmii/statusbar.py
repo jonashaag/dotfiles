55e4e833f48097daeffb96cbd4a4cb5ea25f9295
Jonas Haag <jonas@lophus.org>
2012-01-15 12:58:53 +0100

    rm .wmii

--- .wmii/statusbar.py
+++ .wmii/statusbar.py
@@ -8,7 +8,6 @@ import moc
 WARN_BATSTATE = 6
 BAT_STATE_FILE = '/proc/acpi/battery/BAT0/state'
 FILES = []
-GERMAN_KEYBOARD = re.compile('pidgin', re.IGNORECASE)
 
 def get_output(cmd):
     return os.popen(cmd).read()
@@ -24,14 +23,16 @@ def frequency(freq):
     return decorator
 
 TIME_FORMAT = '%a %b %d %H:%M:%S'
-@frequency(2)
 @register
 def clock():
     now = datetime.datetime.now()
     return now.strftime(TIME_FORMAT)
 
+@register
+def volume():
+    return re.findall('\d+%', get_output('amixer get PCM'))[0]
+
 MOCP_FORMAT_STRING = "%(artist)s -- %(songtitle)s %(currenttime)s"
-@frequency(2)
 @register
 def mocp_state():
     try:
@@ -55,43 +56,7 @@ def mocp_state():
 @frequency(10)
 @register
 def battery_state():
-    try:
-        batstate = get_output('acpitool -b').split(':', 1)[1].strip()
-    except IndexError:
-        return 'running on AC'
-    if 'discharging' in batstate:
-        try:
-            if float(batstate.split(' ')[1].rstrip(',%')) < WARN_BATSTATE:
-                batstate = 'WARNING: %s' % batstate
-        except (IndexError, ValueError, TypeError):
-            pass
-        rate = [line for line in open(BAT_STATE_FILE).read().split('\n')
-                if 'present rate' in line][0].split('rate:')[1].strip()
-        return batstate + ' (%s)' % rate
-    return batstate
-
-def _get_keyboard_layout():
-    x = os.popen('setxkbmap -print').read()
-    if '+de' in x:
-        if '+de(nodead' in x:
-            return 'de nodeadkeys'
-        return 'de'
-    return 'us'
-
-#@register
-def keyboard_layout():
-    props = os.popen('wmiir read /client/sel/props 2>/dev/null').read()
-    current = _get_keyboard_layout()
-    if GERMAN_KEYBOARD.match(props):
-        if current[:2] == 'us':
-            os.system('setxkbmap de')
-            return 'de'
-        return current
-    else:
-        if current == 'de':
-            os.system('setxkbmap us')
-            return 'us'
-        return current
+    return get_output('acpi').split(':', 1)[1].strip().lower()
 
 def main():
     WMII_UPDATE_CMD = 'echo -n "%s" | wmiir create /rbar/%s%s >/dev/null 2>&1'
@@ -103,12 +68,12 @@ def main():
         for index, (file, callback) in enumerate(FILES):
             if i % getattr(callback, '_frequency', 1) != 0:
                 continue
-            retval = os.system(WMII_UPDATE_CMD % (callback(), index, file))
+            retval = os.system(WMII_UPDATE_CMD % (' %s  ' % callback().strip(), index, file))
             if retval == 256:
                 print "wmii seems unavailable, refusing to work."
                 exit(1)
 
-        sleep(0.5)
+        sleep(1)
 
 if __name__ == '__main__':
     main()
